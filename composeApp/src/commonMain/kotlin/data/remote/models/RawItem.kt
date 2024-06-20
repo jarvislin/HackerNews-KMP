82f52c1342bacf8c659f4cecad5d74f84f108916
Jarvis Lin <admin@jarvislin.com>
2024-06-20 19:10:24 +0200

    Fix issues in the raw item

--- composeApp/src/commonMain/kotlin/data/remote/models/RawItem.kt
+++ composeApp/src/commonMain/kotlin/data/remote/models/RawItem.kt
@@ -1,10 +1,12 @@
 package data.remote.models
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 class RawItem(
     @SerialName("type")
     val type: String,
     @SerialName("url")
-    val url: String? // for determining the real type of the item, Ask and Story have the same type
+    val url: String? = null // for determining the real type of the item, Ask and Story have the same type
 )
