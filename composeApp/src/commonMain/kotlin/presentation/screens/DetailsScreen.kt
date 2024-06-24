56535b2527252efe60551477d4a1ae5ece1f5702
Jarvis Lin <admin@jarvislin.com>
2024-06-24 12:10:32 +0200

    Implement the UI of details screen

--- composeApp/src/commonMain/kotlin/presentation/screens/DetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/DetailsScreen.kt
@@ -1,12 +1,226 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package presentation.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.UriHandler
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.getScreenModel
+import com.mohamedrejeb.richeditor.model.rememberRichTextState
+import com.mohamedrejeb.richeditor.ui.material3.RichText
+import domain.models.Comment
 import domain.models.Item
+import domain.models.getCommentCount
+import domain.models.getCommentIds
+import domain.models.getFormatedTime
+import domain.models.getPoint
+import domain.models.getText
+import domain.models.getTitle
+import domain.models.getUrl
+import domain.models.getUserName
+import hackernewskmp.composeapp.generated.resources.Res
+import hackernewskmp.composeapp.generated.resources.link
+import hackernewskmp.composeapp.generated.resources.message
+import io.github.aakira.napier.Napier
+import io.ktor.http.Url
+import kotlinx.serialization.json.Json
+import org.jetbrains.compose.resources.painterResource
+import org.koin.compose.koinInject
+import presentation.viewmodels.DetailsViewModel
 
-class DetailsScreen(private val item: Item) : Screen {
+class DetailsScreen(private val itemJson: String) : Screen {
     @Composable
     override fun Content() {
-        TODO("Not yet implemented")
+        val json = koinInject<Json>()
+        val item = Item.from(json, itemJson) ?: throw IllegalStateException("Item is null")
+        MaterialTheme {
+            Scaffold(topBar = { DetailsTopBar() }) { padding ->
+                CommentList(item, padding, getScreenModel<DetailsViewModel>())
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun DetailsTopBar() {
+    TopAppBar(
+        title = { },
+    )
+}
+
+@Composable
+fun PreviewWidget(url: String) {
+    Text(text = url)
+}
+
+@Composable
+fun CommentList(item: Item, paddingValues: PaddingValues, viewModel: DetailsViewModel = koinInject()) {
+    val comments by viewModel.comments
+    val listState = rememberLazyListState()
+    val isLoading by viewModel.isLoading
+
+    if (isLoading.not()) {
+        viewModel.loadComments(item.getCommentIds())
+    }
+
+    LazyColumn(
+        state = listState,
+        modifier = Modifier.padding(
+            top = paddingValues.calculateTopPadding(),
+            bottom = paddingValues.calculateBottomPadding()
+        )
+    ) {
+        item { ContentWidget(item) }
+        items(comments.size) { index ->
+            CommentWidget(comments[index])
+        }
+    }
+}
+
+@Composable
+fun ContentWidget(item: Item) {
+    val richTextState = rememberRichTextState()
+    richTextState.setHtml(item.getText() ?: "No content")
+
+    Column {
+        Text(
+            text = item.getTitle(), Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+            fontFamily = MaterialTheme.typography.titleLarge.fontFamily
+        )
+        item.getUrl()?.let { url ->
+            Row {
+                Icon(
+                    painter = painterResource(Res.drawable.link),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.padding(start = 16.dp)
+                        .align(Alignment.CenterVertically)
+                )
+                Text(
+                    Url(url).host,
+                    Modifier.padding(horizontal = 4.dp),
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                )
+            }
+        }
+
+        Row(modifier = Modifier.padding(vertical = 8.dp)) {
+            Card(
+                modifier = Modifier.padding(start = 16.dp),
+            ) {
+                Text(
+                    text = "${item.getPoint()} points",
+                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                )
+            }
+            Card(
+                modifier = Modifier.padding(start = 8.dp),
+            ) {
+                Row {
+                    Icon(
+                        painter = painterResource(Res.drawable.message),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.padding(start = 8.dp).align(Alignment.CenterVertically)
+                    )
+                    Text(
+                        text = "${item.getCommentCount()}",
+                        fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                        modifier = Modifier.padding(start = 4.dp, end = 8.dp),
+                    )
+                }
+            }
+        }
+        Text(
+            text = "${item.getUserName()} published on ${item.getFormatedTime()}",
+            modifier = Modifier.padding(horizontal = 16.dp).padding(top = 8.dp),
+            fontSize = MaterialTheme.typography.bodyMedium.fontSize
+        )
+        RichText(
+            state = richTextState,
+            fontFamily = MaterialTheme.typography.titleMedium.fontFamily,
+            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+            fontStyle = MaterialTheme.typography.bodyLarge.fontStyle,
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun CommentWidget(comment: Comment) {
+    val paddingStart = 16.dp * comment.depth
+    val localUriHandler = LocalUriHandler.current
+    val richTextState = rememberRichTextState()
+    richTextState.setHtml(comment.getText() ?: "No content")
+
+    val uriHandler by remember {
+        mutableStateOf(object : UriHandler {
+            override fun openUri(uri: String) {
+                localUriHandler.openUri(decodeUrl(uri))
+            }
+        })
+    }
+
+    Card(
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            .padding(start = paddingStart)
+            .fillMaxWidth(),
+    ) {
+        Column {
+            Text(
+                text = comment.getUserName(),
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize
+            )
+            CompositionLocalProvider(LocalUriHandler provides uriHandler) {
+                RichText(
+                    state = richTextState,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                    fontFamily = MaterialTheme.typography.titleMedium.fontFamily
+                )
+            }
+            Text(
+                text = "${comment.getFormatedTime()}",
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize
+            )
+        }
+    }
+}
+
+private fun decodeUrl(url: String): String {
+    val entityPattern = Regex("&#x([0-9A-Fa-f]+);")
+    return url.replace(entityPattern) { matchResult ->
+        val codePoint = matchResult.groupValues[1].toInt(16)
+        CharArray(1) { codePoint.toChar() }.concatToString()
     }
 }
