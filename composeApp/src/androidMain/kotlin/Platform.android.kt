0bef32d358f9a7fd228a7485261fff00f0a8d595
Jarvis Lin <admin@jarvislin.com>
2024-07-02 18:18:52 +0200

    Update the Typography settings for two platforms

--- composeApp/src/androidMain/kotlin/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/Platform.android.kt
@@ -1,17 +1,21 @@
 import android.os.Build
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import ui.appTypography
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
-
     @Composable
     override fun getScreenWidth(): Float =
         with(LocalDensity.current) { LocalConfiguration.current.screenWidthDp.dp.toPx() }
 
     override fun isAndroid(): Boolean = true
+
+    @Composable
+    override fun getTypography(): Typography = appTypography()
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/iosMain/kotlin/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/Platform.ios.kt
@@ -1,17 +1,23 @@
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalWindowInfo
 import platform.UIKit.UIDevice
+import ui.baseline
 
+@ExperimentalComposeUiApi
 class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 
-    @OptIn(ExperimentalComposeUiApi::class)
     @Composable
     override fun getScreenWidth(): Float =
         LocalWindowInfo.current.containerSize.width.toFloat()
 
     override fun isAndroid(): Boolean = false
+
+    @Composable
+    override fun getTypography(): Typography = baseline
 }
 
+@ExperimentalComposeUiApi
 actual fun getPlatform(): Platform = IOSPlatform()
